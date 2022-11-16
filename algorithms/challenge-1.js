841c60aa92340d0f51606625013116dba3d76d2d
USER <user@USERs-MacBook-Pro.local>
2022-11-16 11:08:07 +0100

    started frontednd

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -5,6 +5,11 @@ function missingNumber(numArr) {
         output.push(index)
       }
     }
+    if (output.length===0) {
+      return false
+    }else if (output.length===1) {
+      return output[0]  
+    }
     return output
 }
 console.log(missingNumber([1,2,3,4,5,6,7,8,9]));
